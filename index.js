1c7b2e8bbcd3695fdf08ced9f57f9737790fb0f7
Jesse Vuontisjärvi <javuon@utu.fi>
2022-04-15 19:22:47 +0300

    Procfile

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ let persons = [
     response.end(JSON.stringify(persons))
   })*/
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
